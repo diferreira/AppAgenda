695251c49767b9dca7623bb095d8245e54ba14d8
diferreira <diegoblks@gmail.com>
2018-02-13 18:36:30 -0200

    criação de array de string, reeferenciei a lista e criei um adapter para transformar s String para View.
    Deixei penas a ListView

--- app/src/main/java/br/com/empresa/appagenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/empresa/appagenda/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.empresa.appagenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String [] alunos = {"Diego", "Monica", "Vanessa", "Walton"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
+
 }
