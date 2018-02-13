b47c6babc949aa8fee04a2e318f80b6c572ddb83
diferreira <diegoblks@gmail.com>
2018-02-13 20:02:25 -0200

    criei um intent para ir pro formulario

--- app/src/main/java/br/com/empresa/appagenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/empresa/appagenda/ListaAlunosActivity.java
@@ -1,9 +1,14 @@
 package br.com.empresa.appagenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -16,6 +21,15 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        Button novoAluno = findViewById(R.id.botao_novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
     }
 
 }
